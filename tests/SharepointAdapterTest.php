109f12bc550c136cca7869367430a4b41f4e57b8
ign_guus <guusleeuw@ignitionnbs.co.uk>
2021-09-22 06:54:59 +0100

    * Leave Flysystem at 1.1, suitable for Laravel 8 * Upgrade to PHPUnit 8.5, and PHP to 7.3 or 8.0 * Apply new method signature

--- tests/SharepointAdapterTest.php
+++ tests/SharepointAdapterTest.php
@@ -18,7 +18,7 @@ class SharepointAdapterTest extends TestCase
 	/** @var \DelaneyMethod\FlysystemSharepoint\SharepointAdapter */
 	protected $sharepointAdapter;
 
-	public function setUp()
+	public function setUp(): void
 	{
 		$this->client = $this->prophesize(Client::class);
 
